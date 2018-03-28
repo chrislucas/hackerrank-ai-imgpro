b59a08d2e9838348e216e25781170f3685a57fe6
Christoffer Lucas <christoffer.luccas@gmail.com>
2018-03-28 20:38:03 -0300

    merging

---
+++ Hough/src/HoughTransformation.java
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * http://www.sunshine2k.de/coding/java/Houghtransformation/HoughTransform.html
+ * */
+
+
+public class HoughTransformation {
+    public static void main(String[] args) {
+
+    }
+}
